a9767e53237609db4dbb7b6ab96b5256a6cf5800
Christian Simon <simon@swine.de>
2016-04-19 16:28:08 +0100

    Adds tar utils for persistence

--- utils/tar.go
+++ utils/tar.go
@@ -5,7 +5,77 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 )
+
+type TarObject struct {
+	Header *tar.Header
+	Body   *[]byte
+}
+
+// tar a list of files and directories
+func tarListOfObjects(objects []TarObject) (tarData []byte, err error) {
+
+	buf := new(bytes.Buffer)
+
+	tarWriter := tar.NewWriter(buf)
+
+	for _, object := range objects {
+		if errWriteHeader := tarWriter.WriteHeader(object.Header); errWriteHeader != nil {
+			err = errWriteHeader
+			return
+		}
+		if _, errWrite := tarWriter.Write(*object.Body); errWrite != nil {
+			err = errWrite
+			return
+		}
+	}
+	err = tarWriter.Close()
+	tarData = buf.Bytes()
+	return
+}
+
+func walkDirToObjects(fullPath string, rootPath string) (objects []TarObject, err error) {
+	err = filepath.Walk(
+		fullPath,
+		func(path string, info os.FileInfo, err error) error {
+			relativePath, err := filepath.Rel(rootPath, path)
+			if err != nil {
+				return err
+			}
+			if len(relativePath) == 0 || relativePath == "." {
+				return nil
+			}
+
+			object := TarObject{
+				Header: &tar.Header{
+					Name:    relativePath,
+					Mode:    int64(info.Mode()),
+					ModTime: info.ModTime(),
+					Size:    info.Size(),
+				},
+			}
+			if !info.IsDir() {
+				object.Header.Typeflag = tar.TypeReg
+
+				// read file content
+				fileBytes, err := ioutil.ReadFile(path)
+				if err != nil {
+					return err
+				}
+				object.Body = &fileBytes
+			} else {
+				object.Header.Typeflag = tar.TypeDir
+			}
+
+			objects = append(objects, object)
+			return nil
+		},
+	)
+	return
+}
 
 func TarFromFile(fileName string, fileBody []byte, fileMode int64) (io.Reader, error) {
 	buf := new(bytes.Buffer)
@@ -17,7 +87,7 @@ func TarFromFile(fileName string, fileBody []byte, fileMode int64) (io.Reader, e
 		Size: int64(len(fileBody)),
 	}
 
-	if err := tarArchive.WriteHeader(fileHeader); err != nil{
+	if err := tarArchive.WriteHeader(fileHeader); err != nil {
 		return nil, err
 	}
 
@@ -49,7 +119,6 @@ func FirstFileFromTar(reader io.Reader) (fileBody []byte, fileName string, err e
 			if err != nil {
 				return
 			}
-
 			fileName = metaData.Name
 			return
 		default:
--- utils/tar_test.go
+++ utils/tar_test.go
@@ -1,17 +1,54 @@
-package utils_test
+package utils
 
 import (
-	"testing"
 	"io/ioutil"
+	"testing"
 
-	"github.com/simonswine/slingshot/utils"
 	"github.com/stretchr/testify/assert"
+	"os"
+	"path"
 )
 
+func createExampleTree(tempDir string) error {
+	err := os.Mkdir(
+		path.Join(tempDir, "testdir"),
+		0750,
+	)
+	if err != nil {
+		return err
+	}
 
-func  TestTarFromFile(t *testing.T) {
+	err = os.Mkdir(
+		path.Join(tempDir, "testempty"),
+		0750,
+	)
+	if err != nil {
+		return err
+	}
 
-	tarReader, err := utils.TarFromFile(
+	ioutil.WriteFile(
+		path.Join(tempDir, "test1.txt"),
+		[]byte("test1"),
+		0600,
+	)
+	if err != nil {
+		return err
+	}
+
+	ioutil.WriteFile(
+		path.Join(tempDir, "testdir", "test2.txt"),
+		[]byte("test2"),
+		0640,
+	)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func TestTarFromFile(t *testing.T) {
+
+	tarReader, err := TarFromFile(
 		"test.txt",
 		[]byte("test123"),
 		0644,
@@ -22,4 +59,37 @@ func  TestTarFromFile(t *testing.T) {
 	assert.Nil(t, err, "Error during tar reading")
 
 	assert.True(t, 50 < len(tarBytes))
+}
+
+func TestWalkDirFromObject(t *testing.T) {
+
+	tempDir, err := ioutil.TempDir("", "gotest")
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = createExampleTree(tempDir)
+	if err != nil {
+		t.Error(err)
+	}
+
+	objects, err := walkDirToObjects(tempDir, tempDir)
+
+	assert.Nil(t, err, "No error happens")
+	assert.Equal(t, 4, len(objects))
+
+	testFiles := 0
+	for _, object := range objects {
+		if object.Header.Name == "test1.txt" {
+			assert.Equal(t, int64(0600), object.Header.Mode)
+			assert.Equal(t, "test1", string(*object.Body))
+			testFiles++
+		}
+		if object.Header.Name == "testdir/test2.txt" {
+			assert.Equal(t, int64(0640), object.Header.Mode)
+			assert.Equal(t, "test2", string(*object.Body))
+			testFiles++
+		}
+	}
+	assert.Equal(t, 2, testFiles, "not engough files found")
 }
